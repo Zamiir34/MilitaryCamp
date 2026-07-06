871d2b1e854f6ed7eddd58b516bd5deeed1f5bed
Mohamed <mohamedabdi9098@gmail.com>
2026-07-06 11:04:04 +0300

    new updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,12 @@
+import '@fontsource/rajdhani/400.css';
+import '@fontsource/rajdhani/500.css';
+import '@fontsource/rajdhani/600.css';
+import '@fontsource/rajdhani/700.css';
+import '@fontsource/share-tech-mono/400.css';
+import '@fontsource/exo-2/300.css';
+import '@fontsource/exo-2/400.css';
+import '@fontsource/exo-2/500.css';
+import '@fontsource/exo-2/600.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
